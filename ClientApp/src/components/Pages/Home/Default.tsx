a0cd06358f2292d3b6ee4a00cb31d7e7d1387bd9
Iavor Orlyov <iavor_orlyov@yahoo.com>
2020-12-20 20:23:36 +0200

    Fixed dependencies

--- ClientApp/src/components/Pages/Home/Default.tsx
+++ ClientApp/src/components/Pages/Home/Default.tsx
@@ -2,29 +2,29 @@ import React, { FunctionComponent } from "react";
 import { useHistory } from "react-router";
 import { EaAccount } from "../../../interfaces/Models";
 
-interface DefaultProps {
-  eaAccounts: EaAccount[];
-}
+type HomeDefaultProps = {
+    eaAccounts: any
+};
 
-const Default: FunctionComponent<DefaultProps> = ({ eaAccounts }) => {
-  const history = useHistory();
+const Default: FunctionComponent<HomeDefaultProps> = ({ eaAccounts }) => {
+    const history = useHistory();
 
-  const handleEaAccountClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    debugger;
-    history.push(`/ea/account/${e.currentTarget.innerText}`);
-  };
+    const handleEaAccountClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        debugger;
+        history.push(`/ea/account/${e.currentTarget.innerText}`);
+    };
 
-  return eaAccounts.map((eaAccount) => {
-    return (
-      <div
-        className="btn btn-dark"
-        onClick={handleEaAccountClick}
-        key={eaAccount.username}
-      >
-        {eaAccount.username}
-      </div>
-    );
-  });
+    return (eaAccounts.map((eaAccount: EaAccount) => {
+        return (
+            <div
+                className="btn btn-dark"
+                onClick={handleEaAccountClick}
+                key={eaAccount.username}
+            >
+                {eaAccount.username}
+            </div>
+        );
+    }));
 };
 
 export default Default;
